66b238c27dccc68b3a8b63697fcaa2f5d25bf19f
Vitalii Denysyuk <denvit1@gmail.com>
2022-05-20 22:51:00 +0300

    move env variables to .env

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const dotenv = require('dotenv');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+dotenv.config();
+console.log(process.env.USER_KEY)
 var app = express();
 
 app.use(logger('dev'));
