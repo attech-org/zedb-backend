4631aa11e558f0810ffc1bd282e0b31327e9a4b0
Vitalii Denysyuk <denvit1@gmail.com>
2022-07-19 13:07:51 +0300

    passport

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ import logRequest from "./middlewares/logRequests";
 import session from 'express-session';
 //import passport from './helpers/google';
 
-// import passport from 'passport';
-// import { OAuth2Strategy } from 'passport-google-oauth'
+import passport from 'passport';
+import { OAuth2Strategy } from 'passport-google-oauth'
 
 dotenv.config();
 export const app = express();
@@ -27,38 +27,38 @@ app.use(session({
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// let userProfile: any;
-// app.set('view engine', 'ejs');
-// app.get('/success', (req, res) => res.send(userProfile));
-// app.get('/error', (req, res) => res.send("error logging in"));
+let userProfile: any;
+app.set('view engine', 'ejs');
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// const GoogleStrategy = OAuth2Strategy;
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GoogleStrategy = OAuth2Strategy;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 
-// passport.serializeUser(function (user, cb) {
-//     cb(null, user);
-// });
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
+});
 
-// passport.deserializeUser(function (obj, cb) {
-//     cb(null, obj);
-// });
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
+});
 
-// passport.use(new GoogleStrategy({
-//     clientID: GOOGLE_CLIENT_ID,
-//     clientSecret: GOOGLE_CLIENT_SECRET,
-//     callbackURL: "http://localhost:3001/users/auth/google/callback"
-// },
-//     function (accessToken, refreshToken, profile, done) {
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3001/users/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, done) {
         
-//         userProfile = profile;
-//         return done(null, userProfile);
-//     }
-// ));
+        userProfile = profile;
+        return done(null, userProfile);
+    }
+));
 
 
 
